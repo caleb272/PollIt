029963df631ffdf718421ae87469cf8620de4cb7
Caleb Martin <martincaleb@gmail.com>
2016-10-09 16:49:48 -0600

    got the d3 charts to render via a psudo dom layer and document.element.innerHTML and React.dangerouslySetInnerHTML but no click events worked so im going to use react-faux-dom

--- client/modules/Poll/components/BarChart/BarChart.js
+++ client/modules/Poll/components/BarChart/BarChart.js
@@ -2,54 +2,35 @@ import React, { PropTypes, Component } from 'react'
 import * as d3 from 'd3'
 
 class BarChart extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      chart: document.createElement('div')
+    }
+  }
+
+
   componentDidMount() {
     this.buildChart(this.props.pollData)
   }
 
-
   buildChart({ entries, id }) {
-    const chart = d3.select(`#${id}`)
-    const padding = { left: 20, top: 10, right: 10, bottom: 20 }
+    // const chart = d3.select(`#${id}`)
+    const chartHTML = document.createElement('div')
+    const chart = d3.select(chartHTML).append('svg')
+    const padding = { left: 30, top: 10, right: 10, bottom: 20 }
     const chartWidth = (600 - (padding.left + padding.right))
     const chartHeight = (400 - (padding.top + padding.bottom))
 
     const xScale = d3.scaleBand()
         .rangeRound([0, chartWidth])
-        .domain(entries.map((entry) => entry.name))
+        .domain(entries.map(entry => entry.name))
         .paddingOuter(0.05)
         .padding(0.05)
     const yScale = d3.scaleLinear()
-        // .range([chartHeight, 0])
-        // .domain([0, d3.max(entries, (entry) => entry.votes.length)])
-
-        // .range([chartHeight, 0])
-        // .domain([d3.max(entries, (entry) => entry.votes.length), 0])
-
         .range([chartHeight, 0])
-        .domain([0, d3.max(entries, (entry) => entry.votes.length)])
-
-    let currentMax = { length: 0, name: '' }
-    for (let i = 0; i < entries.length; i++) {
-      const length = entries[i].votes.length
-      if (currentMax.length < length) {
-        currentMax = {
-          length,
-          name: entries[i].name
-        }
-      }
-    }
-
-    let currentMin = { length: currentMax.length, name: currentMax.name }
-    for (let i = 0; i < entries.length; i++) {
-      const length = entries[i].votes.length
-      if (currentMin.length > length) {
-        currentMin = {
-          length,
-          name: entries[i].name
-        }
-      }
-    }
-    console.log('max:', currentMax, ' min:', currentMin)
+        .domain([0, d3.max(entries, entry => entry.votes.length)])
 
     const xAxis = d3.axisBottom(xScale)
     const yAxis = d3.axisLeft(yScale)
@@ -63,35 +44,51 @@ class BarChart extends Component {
         .attr('width', chartWidth)
         .attr('height', chartHeight)
 
+    this.setState({ chart: chartHTML })
+
     innerChart.selectAll('rect')
         .data(entries).enter()
       .append('rect')
         .attr('fill', 'orange')
         .attr('width', xScale.bandwidth())
-        .attr('height', (entry) => {
-          console.log('name:', entry.name, '    pos:', yScale(entry.votes.length))
-          return chartHeight - yScale(entry.votes.length)
-        })
+        .attr('height', (entry) => chartHeight - yScale(entry.votes.length))
         .attr('x', entry => xScale(entry.name))
         .attr('y', entry => (yScale(entry.votes.length)))
-
-    console.log('fuck', yScale(currentMax.length))
+        .on('click', bar => console.log(bar))
 
     innerChart.append('g')
+        .classed('.x.axis', true)
         .attr('transform', `translate(0, ${chartHeight})`)
         .call(xAxis)
 
     innerChart.append('g')
+        .classed('.y.axis', true)
         .attr('transform', 'translate(0, 0)')
         .call(yAxis)
+
+    function onBarClicked(bar) {
+      console.log('clicked')
+      const duration = 750
+      bar.votes.push(Math.floor(Math.random() * 9999999999))
+      xScale.domain(entries.map(entry => entry.name))
+      yScale.domain([0, d3.max(entries, entry => entry.votes.length)])
+
+      innerChart.select('.x.axis')
+          .call(xAxis)
+      innerChart.select('.y.axis')
+          .call(yAxis)
+    }
+
+    this.setState({ chart: chartHTML })
   }
 
 
+  // <svg id={this.props.pollData.id}>{this.state.updates}</svg>
   render() {
     return (
       <div> <center>
         <h3>{this.props.pollData.name}</h3>
-        <svg id={this.props.pollData.id}></svg>
+        <div dangerouslySetInnerHTML={{ __html: (this.state.chart.innerHTML) }}></div>
       </center> </div>
     )
   }
