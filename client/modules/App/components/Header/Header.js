38a920515dd8a7f9de158b36f50708bc78d6ae0a
Caleb Martin <martincaleb@gmail.com>
2016-10-15 11:00:53 -0600

    Modified the site title in the Header.js

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,28 +1,18 @@
-import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
-import { FormattedMessage } from 'react-intl';
+import React from 'react'
+import { Link } from 'react-router'
+import { FormattedMessage } from 'react-intl'
 
 import LoginButton from '../LoginButton/LoginButton'
 
 // Import Style
-import styles from './Header.css';
+import styles from './Header.css'
 
 export function Header(props, context) {
-  const languageNodes = props.intl.enabledLanguages.map(
-    lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
-  );
-
   return (
     <div className={styles.header}>
-      <div className={styles['language-switcher']}>
-        <ul>
-          <li><FormattedMessage id="switchLanguage" /></li>
-          {languageNodes}
-        </ul>
-      </div>
       <div className={styles.content}>
         <h1 className={styles['site-title']}>
-          <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
+          <Link to="/" id="siteTitle">POLL IT</Link>
         </h1>
         {
           context.router.isActive('/', true)
@@ -31,16 +21,11 @@ export function Header(props, context) {
         }
       </div>
     </div>
-  );
+  )
 }
 
 Header.contextTypes = {
   router: React.PropTypes.object,
-};
-
-Header.propTypes = {
-  switchLanguage: PropTypes.func.isRequired,
-  intl: PropTypes.object.isRequired,
-};
+}
 
 export default Header;
