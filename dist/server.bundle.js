48a40dc5235bcfc03302acf7f95468dc8b63a065
Caleb Martin <martincaleb@gmail.com>
2016-10-29 01:29:38 -0600

    heroku BS again

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -3549,13 +3549,13 @@
 	  GITHUB_CALLBACK_URL: 'http://192.168.1.8:8000/auth/github/callback',
 	  EXPRESS_SESSION_SECRET: 'calebmartin',
 
+	  MONGO_URL: 'mongodb://caleb-db:123Dog@ds023560.mlab.com:23560/fcc-project-database',
+
 	  GOOGLE_CLIENT_ID: '1069419221190-bj809935dmpantne6hii5re7js99sv2t.apps.googleusercontent.com',
 	  GOOGLE_CLIENT_SECRET: 'iPBdIi9Naq7K4Ze38CuOUFvg',
 
 	  BITLY_SHORTENED_CALLBACK_URL: 'http://bit.ly/2dRoLMw',
-	  GOOGLE_CALLBACK_URL: 'http://bit.ly/2dRoLMw',
-
-	  MONGO_URL: 'mongodb://caleb-db:123Dog@ds023560.mlab.com:23560/fcc-project-database'
+	  GOOGLE_CALLBACK_URL: 'http://bit.ly/2dRoLMw'
 	};
 
 /***/ },
